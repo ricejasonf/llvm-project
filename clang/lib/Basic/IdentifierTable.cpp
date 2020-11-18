c06b0521b270cef2e480c2d62d82b6c453e450f9
Jason Rice <ricejasonf@gmail.com>
2020-11-18 10:34:41 -0800

    [Heavy] Adds missing deps for heavy_scheme

--- clang/lib/Basic/IdentifierTable.cpp
+++ clang/lib/Basic/IdentifierTable.cpp
@@ -164,6 +164,8 @@ static KeywordStatus getKeywordStatusHelper(const LangOptions &LangOpts,
     return KS_Unknown;
   case KEYFIXEDPOINT:
     return LangOpts.FixedPoint ? KS_Enabled : KS_Disabled;
+  case KEYHEAVY:
+    return LangOpts.Heavy ? KS_Enabled: KS_Unknown;
   default:
     llvm_unreachable("Unknown KeywordStatus flag");
   }
