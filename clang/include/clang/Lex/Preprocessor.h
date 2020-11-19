2b8404cc96ed9b451390d47b6dcdaeb596ceb255
Jason Rice <ricejasonf@gmail.com>
2020-11-19 15:38:17 -0800

    [Heavy] Reduces clang deps in Parser/Lexer

--- clang/include/clang/Lex/Preprocessor.h
+++ clang/include/clang/Lex/Preprocessor.h
@@ -74,7 +74,7 @@ class ExternalPreprocessorSource;
 class FileEntry;
 class FileManager;
 class HeaderSearch;
-class HeavySchemeLexer;
+class EmbeddedLexer;
 class MacroArgs;
 class PragmaHandler;
 class PragmaNamespace;
@@ -3126,12 +3126,8 @@ private:
   }
 
 public:
-  void InitHeavySchemeLexer();
-  void FinishHeavySchemeLexer();
-  void LexHeavyScheme(Token& Tok);
-
-private:
-  std::unique_ptr<HeavySchemeLexer> TheHeavySchemeLexer;
+  void InitEmbeddedLexer(EmbeddedLexer& EL);
+  void FinishEmbeddedLexer(EmbeddedLexer& EL);
 };
 
 /// Abstract base class that describes a handler that will receive
