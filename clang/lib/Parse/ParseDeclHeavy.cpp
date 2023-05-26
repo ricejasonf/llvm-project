2f64d60699e047cfda2463ce3eeb48a4e9c1b0ad
Jason Rice <ricejasonf@gmail.com>
2023-05-25 17:30:37 -0700

    [Heavy] Add provisional expr-eval function

--- clang/lib/Parse/ParseDeclHeavy.cpp
+++ clang/lib/Parse/ParseDeclHeavy.cpp
@@ -16,6 +16,7 @@
 #include "heavy/HeavyScheme.h"
 #include "heavy/Value.h"
 #include "clang/Parse/Parser.h"
+#include "clang/AST/Expr.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/PrettyDeclStackTrace.h"
 #include "clang/Basic/CharInfo.h"
@@ -32,6 +33,7 @@ bool HEAVY_CLANG_IS_LOADED = false;
 heavy::ExternLambda<1> HEAVY_CLANG_VAR(diag_error) = {};
 heavy::ExternLambda<1> HEAVY_CLANG_VAR(hello_world) = {};
 heavy::ExternLambda<1> HEAVY_CLANG_VAR(write_lexer) = {};
+heavy::ExternLambda<1, sizeof(void*)*2> HEAVY_CLANG_VAR(expr_eval) = {};
 
 namespace {
 // Convert to a clang::SourceLocation or an invalid location if it
@@ -81,25 +83,13 @@ class LexerWriter {
   }
 
 public:
-  static auto CreateDefaultFn() {
-    return [](heavy::Context& C, heavy::ValueRefs Args) {
-      C.RaiseError("token buffer is not initialized");
-    };
-  }
-
   LexerWriter(clang::Parser& P, heavy::HeavyScheme& HS)
     : Parser(P),
       HeavyScheme(HS),
       TokenBuffer(nullptr)
-  {
-    HEAVY_CLANG_VAR(write_lexer) = [this](heavy::Context& C,
-                                          heavy::ValueRefs Args) mutable {
-      this->operator()(C, Args);
-    };
-  }
+  { }
 
   ~LexerWriter() {
-    HEAVY_CLANG_VAR(write_lexer) = CreateDefaultFn();
     Capacity = 0;
     Size = 0;
   }
@@ -135,14 +125,20 @@ public:
   void FlushTokens() {
     if (Size == 0) return;
     Parser.getPreprocessor().EnterTokenStream(std::move(TokenBuffer), Size,
-                    /*DisableMacroExpansion=*/false,
+                    /*DisableMacroExpansion=*/true,
                     /*IsReinject=*/false);
+    Capacity = 0;
+    Size = 0;
   }
 };
 
-void LoadBuiltinModule(clang::Parser& P) {
+void LoadBuiltinModule(clang::Parser& P, heavy::HeavyScheme* HS) {
+  // FIXME We are capturing Parser and storing it in globals.
+  //       This is a problem if we have multiple CompilerInvocations.
+  //       There must be a way to store "globals" with Context if
+  //       they have state.
+  //       (This is a problem for static modules in general.)
   auto diag_error = [&](heavy::Context& C, heavy::ValueRefs Args) {
-    // TODO automate argument checking somehow
     if (Args.size() != 1) {
       C.RaiseError("invalid arity to function", C.getCallee());
       return;
@@ -162,19 +158,101 @@ void LoadBuiltinModule(clang::Parser& P) {
     C.Cont();
   };
 
-  // LexerWriter swaps this out every time it is run.
-  auto write_lexer = LexerWriter::CreateDefaultFn();
+  auto expr_eval = [&](heavy::Context& C, heavy::ValueRefs Args) {
+    if (Args.size() != 1) {
+      C.RaiseError("invalid arity to function", C.getCallee());
+      return;
+    }
+    if (!isa<heavy::String>(Args[0])) {
+      C.RaiseError("expecting string", C.getCallee());
+      return;
+    }
+    llvm::StringRef Source = cast<heavy::String>(Args[0])->getView();
+
+
+    // Lex and expand.
+    clang::Lexer Lexer(clang::SourceLocation(), P.getLangOpts(),
+                       Source.data(), Source.data(), &(*(Source.end())));
+    LexerWriter TheLexerWriter(P, *HS);
+    TheLexerWriter.FlushTokens();
+
+    // Parse the expression.
+    clang::ExprResult ExprResult = P.ParseExpression();
+
+    // TODO
+    // Make sure the macro expansion is popped off the include stack.
+
+    // Process the parsing result if any.
+    if (ExprResult.isInvalid()) {
+      return C.RaiseError("clang expression parsing failed");
+    }
+    clang::Expr* Expr = ExprResult.get();
+
+    // ConstantExpr eval.
+    clang::Expr::EvalResult EvalResult;
+    if (!Expr->EvaluateAsRValue(EvalResult,
+          P.getActions().getASTContext())) {
+      // The evaluation failed.
+      // TODO Have Clang emit the diagnostics (ie From EvalResult.Diag)
+      C.RaiseError("clang expression evaluation failed");
+      return;
+    }
+
+    // Convert EvalResult/APValue to Scheme value.
+    heavy::Value Result;
+    switch (EvalResult.Val.getKind()) {
+      case APValue::None:
+      case APValue::Indeterminate: {
+        // ... Or maybe we allow errors. (ie like SFINAE)
+        Result = heavy::Undefined();
+        C.RaiseError("clang expression evaluation failed");
+        return;
+      }
+      case APValue::Int: {
+        llvm::APSInt Int = EvalResult.Val.getInt();
+        if (Expr->getType()->isBooleanType()) {
+          Result = heavy::Bool(Int.getBoolValue());
+        } else if (Int.isSignedIntN(32)) {
+          Result = heavy::Int(Int.getZExtValue());
+        }
+        break;
+      }
+      // TODO Support these value types.
+      case APValue::Float:
+      case APValue::FixedPoint:
+      case APValue::ComplexInt:
+      case APValue::ComplexFloat:
+      case APValue::Vector:
+      case APValue::Array:
+      case APValue::Struct:
+
+      // Will not support.
+      case APValue::LValue:
+      case APValue::Union:
+      case APValue::MemberPointer:
+      case APValue::AddrLabelDiff:
+      default:
+        // Do nothing.
+      break;
+    }
+
+    if (Result) {
+      C.Cont(Result);
+    } else {
+      C.RaiseError("unsupported result type");
+    }
+  };
 
   HEAVY_CLANG_VAR(diag_error)   = diag_error;
   HEAVY_CLANG_VAR(hello_world)  = hello_world;
-  HEAVY_CLANG_VAR(write_lexer)  = write_lexer;
+  HEAVY_CLANG_VAR(expr_eval)    = expr_eval;
 }
 } // end anon namespace
 
 bool Parser::ParseHeavyScheme() {
   if (!HeavyScheme) {
     HeavyScheme = std::make_unique<heavy::HeavyScheme>();
-    LoadBuiltinModule(*this);
+    LoadBuiltinModule(*this, HeavyScheme.get());
     HeavyScheme->RegisterModule(HEAVY_CLANG_LIB_STR, HEAVY_CLANG_LOAD_MODULE);
   }
 
@@ -206,6 +284,10 @@ bool Parser::ParseHeavyScheme() {
 
 
   LexerWriter TheLexerWriter(*this, *HeavyScheme);
+  HEAVY_CLANG_VAR(write_lexer) = [&](heavy::Context& C,
+                                     heavy::ValueRefs Args) mutable {
+    TheLexerWriter.operator()(C, Args);
+  };
   heavy::TokenKind Terminator = heavy::tok::r_brace;
   HeavyScheme->ProcessTopLevelCommands(SchemeLexer,
                                        ErrorHandler,
@@ -214,6 +296,10 @@ bool Parser::ParseHeavyScheme() {
   // Return control to C++ Lexer
   PP.FinishEmbeddedLexer(SchemeLexer.GetByteOffset());
   TheLexerWriter.FlushTokens();
+  HEAVY_CLANG_VAR(write_lexer) = [&](heavy::Context& C,
+                                     heavy::ValueRefs Args) mutable {
+    C.RaiseError("lexer writer is not initialized");
+  };
 
   // The Lexers position has been changed
   // so we need to re-prime the look-ahead
